3fd4ddfbd0f374fd89c46cd5c3f287115d8b72eb
prgers <prgers@163.com>
2021-01-14 14:56:28 +0800

    上传代码

--- aliyun-oss/src/test/java/OSSTest.java
+++ aliyun-oss/src/test/java/OSSTest.java
@@ -11,8 +11,8 @@ public class OSSTest {
         // Endpoint以杭州为例，其它Region请按实际情况填写。
         String endpoint = "http://oss-cn-hangzhou.aliyuncs.com";
         // 阿里云主账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建RAM账号。
-        String accessKeyId = "LTAI4G5jHZwtKSTFMc64rbZE";
-        String accessKeySecret = "gOit0nevWmBt6j8IvQSiX4baciSSa8";
+        String accessKeyId = "";
+        String accessKeySecret = "";
 
         // 创建OSSClient实例。
         OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
